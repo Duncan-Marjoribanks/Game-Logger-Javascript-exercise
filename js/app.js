ad151b1fb136242ff19adb01ed2af79eec306c92
Duncan <marjoribanks.d@gmail.com>
2018-06-25 07:50:42 +0100

    added a small quantity of css

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
+
 const form = document.querySelector('#games-logged-form');
 
-const handleSubmit = function(event) {
+
+
+const handleSubmit = function (event) {
+
   event.preventDefault();
 
-  const nameOfGame = document.querySelector('#name');
-  const winnerOfGame = document.querySelector('#winner');
+  const nameOfGame = this.querySelector('#name');
+  const winnerOfGame = this.querySelector('#winner');
 
   const nameOfGameText = nameOfGame.value;
   const winnerOfGameText = winnerOfGame.value;
 
-  const newGameLogged = document.createElement('p');
-  const gameRecordString = `Game Played: ${nameOfGameText} `;
+  const newGameLogged = document.createElement('li');
+  const gameLoggedString = `Game Played: ${nameOfGameText} `;
   const winnerRecordString = `Winner: ${winnerOfGameText}`;
-  newGameLogged.textContent = gameRecordString;
+  newGameLogged.textContent = gameLoggedString;
 
   const gamesPlayed = document.querySelector('#games-played');
   gamesPlayed.appendChild(newGameLogged);
